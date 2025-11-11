7a081aace304b73c05958724f4339b371cf59d29
Shariful Islam <sharifahmadwd@gmail.com>
2025-11-11 15:11:15 +0600

    users data sdded

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ const client = new MongoClient(uri, {
 });
 
 let moviesCollection;
+let usersCollection;
 
 async function run() {
   await client.connect();
   const db = client.db("movieMasterStudio");
   moviesCollection = db.collection("movies");
+  usersCollection = db.collection("users");
 
   await client.db("admin").command({ ping: 1 });
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -31,11 +33,15 @@ async function run() {
 
 run().catch(console.dir);
 
+/* ============ Movies Routes ============== */
+
 // Get all movies
 app.get("/movies", async (req, res) => {
-  // const moviefield = { title: 1, director: 1, rating: 1 }; 
-  // const cursor = moviesCollection.find().sort({ rating: -1 }).skip(5).limit(2).project(moviefield);
-  const cursor = moviesCollection.find();
+  const email = req.query.email;
+  let query = {};
+  if (email) query.email = email;
+
+  const cursor = moviesCollection.find(query);
   const result = await cursor.toArray();
   res.send(result);
 });
@@ -60,10 +66,7 @@ app.patch("/movies/:id", async (req, res) => {
   const updatedMovie = req.body;
   const query = { _id: new ObjectId(id) };
   const update = {
-    $set: {
-      title: updatedMovie.title,
-      director: updatedMovie.director,
-    },
+    $set: { title: updatedMovie.title, director: updatedMovie.director },
   };
   const result = await moviesCollection.updateOne(query, update);
   res.send(result);
@@ -77,12 +80,63 @@ app.delete("/movies/:id", async (req, res) => {
   res.send(result);
 });
 
-// Default route
+/* =========== Users Routes =========== */
+
+// Get all users
+app.get("/users", async (req, res) => {
+  const email = req.query.email;
+  let query = {};
+  if (email) query.email = email;
+
+  const cursor = usersCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+// Get single user by ID
+app.get("/users/:id", async (req, res) => {
+  const user = await usersCollection.findOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(user);
+});
+
+// Add a new user
+app.post("/users", async (req, res) => {
+  const result = await usersCollection.insertOne(req.body);
+  res.send(result);
+});
+
+// Update a user
+app.patch("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedUser = req.body;
+  const query = { _id: new ObjectId(id) };
+  const update = {
+    $set: {
+      name: updatedUser.name,
+      email: updatedUser.email,
+      role: updatedUser.role,
+      age: updatedUser.age,
+      photo: updatedUser.photo,
+    },
+  };
+  const result = await usersCollection.updateOne(query, update);
+  res.send(result);
+});
+
+// Delete a user
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Smart Server is Running on port ${port}`);
 });
